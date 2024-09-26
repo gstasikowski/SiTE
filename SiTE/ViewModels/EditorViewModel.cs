01437c0f478a50abcb0ab3437527a23e1159ca09
gstasikowski <stasikowski.gerard@tutanota.com>
2024-09-26 17:14:18 +0200

    Added note list binding

--- SiTE/ViewModels/EditorViewModel.cs
+++ SiTE/ViewModels/EditorViewModel.cs
@@ -1,7 +1,12 @@
+using System.Collections.ObjectModel;
+
 namespace SiTE.ViewModels
 {
 	public class EditorViewModel : ViewModelBase
 	{
-		
+		public ObservableCollection<Models.NoteModel> NoteList
+		{
+			get { return Logic.Refs.dataBank.NoteList; }
+		}
 	}
 }
