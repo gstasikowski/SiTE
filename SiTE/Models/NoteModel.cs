26bcb355d4bceab1696572abe4337d1a22a4f000
gstasikowski <stasikowski.gerard@tutanota.com>
2024-09-27 19:26:45 +0200

    NoteModel preparations for binding

--- SiTE/Models/NoteModel.cs
+++ SiTE/Models/NoteModel.cs
@@ -1,13 +1,80 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace SiTE.Models
 {
-    public class NoteModel
+    public class NoteModel : INotifyPropertyChanged
     {
-        public Guid ID { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime Modified { get; set; }
+        private Guid _id;
+        private string _title;
+        private string _content;
+        private DateTime _created;
+        private DateTime _modified;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+        public NoteModel()
+        {
+            ID = System.Guid.Empty;
+            Title = string.Empty;
+            Content = string.Empty;
+        }
+
+        public Guid ID
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+				OnPropertyChanged();
+            }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+				OnPropertyChanged();
+            }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+				OnPropertyChanged();
+            }
+        }
+
+        public DateTime Created
+        {
+            get { return _created; }
+            set
+            {
+                _created = value;
+				OnPropertyChanged();
+            }
+        }
+
+        public DateTime Modified
+        {
+            get { return _modified; }
+            set
+            {
+                _modified = value;
+				OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		    System.Console.WriteLine("[NoteModel] ==> changing \'" + name + "\'");
+		}
     }
 }
